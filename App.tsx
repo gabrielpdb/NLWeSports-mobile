1e864089497891c2978cd7414b6796270c7cc220
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-09-12 00:58:15 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello React Native</Text>
+      <Button title="Butão" />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
